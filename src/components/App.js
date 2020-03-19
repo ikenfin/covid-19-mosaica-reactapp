6ff96051d958b868251a323f720b129523e20ec2
ikenfin <ikenfin@gmail.com>
2020-03-20 02:13:22 +0400

    hide app if no data

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import WorldData from './WorldData'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState({})
+  const [data, setData] = useState(null)
   const [country, setCountry] = useState('Russia')
   const [countryObj, setCountryObj] = useState(null)
 
@@ -27,24 +27,27 @@ function App() {
   }, [])
 
   return (
-    <div className="covid19-App">
-      <div className="covid19-App__header">
-        <CountrySelect
-          value={ country }
-          countries={ data.countries }
-          onChange={ onCountryChange }
-        />
+    <React.Fragment>
+      { data ? (
+      <div className="covid19-App">
+        <div className="covid19-App__header">
+          <CountrySelect
+            value={ country }
+            countries={ data.countries }
+            onChange={ onCountryChange }
+          />
 
-        { data.updated ? (<span class="covid19-App__updated">Данные обновлены: { formatUpdateDate(data.updated) }</span>) : '' }
+          { data.updated
+            ? (<span class="covid19-App__updated">Данные обновлены: { formatUpdateDate(data.updated) }</span>)
+            : ''
+          }
+        </div>
+
+        { countryObj ? (<CountryData data={ countryObj } />) : '' }
+        { data.data ? <WorldData data={ data.data } /> : '' }
       </div>
-
-      { countryObj ? (
-      <CountryData
-        data={ countryObj }
-      />) : '' }
-
-      { data.data ? <WorldData data={ data.data } /> : '' }
-    </div>
+    ) : '' }
+    </React.Fragment>
   )
 }
 
