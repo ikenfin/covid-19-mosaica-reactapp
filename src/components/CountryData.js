dd217deb0b4413401dbeb36c7bfa0faab254f5ee
ikenfin <ikenfin@gmail.com>
2020-03-20 15:22:33 +0400

    :art: styling app

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -7,16 +7,28 @@ import './CountryData.css'
 export default function CountryData ({ data }) {
   return <div className="covid19-countryData">
     <div className="covid19-countryData__infoblock covid19-countryData__active">
-      <h3>{ data.activeCases } <span className="covid19-countryData__infoblock_dynamic">{ data.casesNew > 0 ? `(+${ data.casesNew })` : '' }</span></h3>
-      <span>Заражений</span>
+      <div className="covid19-countryData__infoblock_content">
+        <div>
+          <h3>{ data.activeCases } <span className="covid19-countryData__infoblock_dynamic">{ data.casesNew > 0 ? `(+${ data.casesNew })` : '' }</span></h3>
+          <span>Заражений</span>
+        </div>
+      </div>
     </div>
     <div className="covid19-countryData__infoblock covid19-countryData__deaths">
-      <h3>{ data.deathsTotal } <span className="covid19-countryData__infoblock_dynamic">{ data.deathsNew > 0 ? `(+${ data.deathsNew })` : '' }</span></h3>
-      <span>Смертей</span>
+      <div className="covid19-countryData__infoblock_content">
+        <div>
+          <h3>{ data.deathsTotal } <span className="covid19-countryData__infoblock_dynamic">{ data.deathsNew > 0 ? `(+${ data.deathsNew })` : '' }</span></h3>
+          <span>Смертей</span>
+        </div>
+      </div>
     </div>
     <div className="covid19-countryData__infoblock covid19-countryData__recovered">
-      <h3>{ data.recoveredTotal }</h3>
-      <span>Выздоровлений</span>
+      <div className="covid19-countryData__infoblock_content">
+        <div>
+          <h3>{ data.recoveredTotal }</h3>
+          <span>Выздоровлений</span>
+        </div>
+        </div>
     </div>
   </div>
 }
